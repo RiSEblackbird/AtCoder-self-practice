cf9548df4ccaa7a54704c1dc2c210a8c4012c613
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-28 20:31:28 +0900

    AC abc229\mainA.py

--- abc229/mainA.py
+++ abc229/mainA.py
@@ -1,5 +1,9 @@
+# A - First Grid
+# https://atcoder.jp/contests/abc229/tasks/abc229_a
+# 提出AC
+# https://atcoder.jp/contests/abc229/submissions/27567759
 S = [list(input()) for _ in range(2)]
-print(S)
+
 if (S[0][1] == "." and S[1][0] == ".") or (S[0][0] == "." and S[1][1] == "."):
     print("No")
 else:
