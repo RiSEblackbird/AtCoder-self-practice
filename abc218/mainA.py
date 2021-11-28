fe10d7c28b63957582dce0fd373a4e1103baea46
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-28 20:17:55 +0900

    AC abc218\mainA.py

---
+++ abc218/mainA.py
@@ -0,0 +1,9 @@
+# A - Weather Forecast
+# https://atcoder.jp/contests/abc218/tasks/abc218_a
+# 提出AC
+# https://atcoder.jp/contests/abc218/submissions/27567562
+
+N = int(input())
+S = list(input())
+ans = "Yes" if S[N-1] == "o" else "No"
+print(ans)
