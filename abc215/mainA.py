d0ad31ecb513b7c5968db52ae5ebcd08b1a0c524
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-28 21:41:33 +0900

    AC abc215\mainA.py

---
+++ abc215/mainA.py
@@ -0,0 +1,7 @@
+# A - Your First Judge
+# https://atcoder.jp/contests/abc215/tasks/abc215_a
+# 提出AC
+# https://atcoder.jp/contests/abc215/submissions/27572997
+S = list(input())
+target = ["H","e","l","l","o",",","W","o","r","l","d","!"]
+print("AC" if S == target else "WA")
