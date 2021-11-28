6e9e7c7b36aa9c26512c68f106b2b30ac19564a4
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-28 17:04:15 +0900

    AC abc228\mainA.py

--- abc228/mainA.py
+++ abc228/mainA.py
@@ -1,3 +1,7 @@
+# A - On and Off
+# https://atcoder.jp/contests/abc228/tasks/abc228_a
+# 提出AC
+# https://atcoder.jp/contests/abc228/submissions/27563521
 s,t,x = map(int, input().split())
 if s < t:
   ans = "Yes" if s <= x < t else "No"
@@ -7,12 +11,3 @@ else:
 print(ans)
 
 # たぶんOK　https://atcoder.jp/contests/abc228/tasks/abc228_a
-# taishi@DESKTOP-GIVL2DT:~/AtCoder$ python3 main.py
-# 23 0 23
-# Yes
-# taishi@DESKTOP-GIVL2DT:~/AtCoder$ python3 main.py
-# 7 20 12
-# Yes
-# taishi@DESKTOP-GIVL2DT:~/AtCoder$ python3 main.py
-# 20 7 12
-# No
