3bf400f86c54a8e49d5097f71a55279bda887e2c
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-28 22:22:20 +0900

    AC abc213\mainA.py (XOR 排他的論理和 ビット演算子)

---
+++ abc213/mainA.py
@@ -0,0 +1,8 @@
+# A - Bitwise Exclusive Or
+# https://atcoder.jp/contests/abc213/tasks/abc213_a
+# 提出AC
+# https://atcoder.jp/contests/abc213/submissions/27575737
+A,B = map(int, input().split())
+print(A ^ B)
+
+# XOR 排他的論理和 ビット演算子
