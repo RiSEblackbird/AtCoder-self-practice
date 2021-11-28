9d849635047362abdd7b80db9d4b22cd40d03994
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-28 17:04:04 +0900

    AC abc227\mainA.py

--- abc227/mainA.py
+++ abc227/mainA.py
@@ -1,12 +1,16 @@
+# A - Last Card
+# https://atcoder.jp/contests/abc227/tasks/abc227_a
+# 提出AC
+# https://atcoder.jp/contests/abc227/submissions/27563613
 n,k,a = map(int, input().split())
 
 if k > (n - a):
   # zk 一巡直後の残りのカード枚数
   zk = k - (n - (a -1))
-  print("zk:{}".format(zk))
+  # print("zk:{}".format(zk))
   sur = zk % n
   ans = n if sur == 0 else sur
-  print("ans:{}".format(ans))
+  # print("ans:{}".format(ans))
 else:
   ans = a + k - 1
 
