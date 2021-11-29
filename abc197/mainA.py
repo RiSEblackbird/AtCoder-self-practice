8e4bd309523a95f900d1fcda9e8a7e9874dd5d1e
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-30 00:20:31 +0900

    Update mainA.py

--- abc197/mainA.py
+++ abc197/mainA.py
@@ -1,7 +1,7 @@
 # A - Rotate 
 # https://atcoder.jp/contests/abc197/tasks/abc197_a
-# 
-# 
+# 提出AC
+# https://atcoder.jp/contests/abc197/submissions/27594484
 s = list(str(input()))
 print(s[1]+s[2]+s[0])
 
