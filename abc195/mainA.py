ef592beb4ddf69e4f8f9fc8120f78a048c7d893e
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-30 00:37:14 +0900

    AC abc195\mainA.py : 倍数

---
+++ abc195/mainA.py
@@ -0,0 +1,8 @@
+# A - Health M Death
+# https://atcoder.jp/contests/abc195/tasks/abc195_a
+# 提出AC
+# https://atcoder.jp/contests/abc195/submissions/27594677
+m,h = map(int, input().split())
+print("Yes" if h % m == 0 else "No")
+
+# 倍数
