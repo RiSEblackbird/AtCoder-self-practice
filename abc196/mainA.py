9b03b368b441851be8e62b7f76637ce0086e1097
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-30 00:32:09 +0900

    AC abc196\mainA.py : 行列

---
+++ abc196/mainA.py
@@ -0,0 +1,9 @@
+# A - Difference Max
+# https://atcoder.jp/contests/abc196/tasks/abc196_a
+# 提出AC
+# https://atcoder.jp/contests/abc196/submissions/27594623
+a,b = map(int, input().split())
+c,d = map(int, input().split())
+print(b-c)
+
+# 行列
