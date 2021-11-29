d0fe9f2c35bab9e40bdf4e7d7d02e8d6c7d2ed55
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-30 01:38:37 +0900

    AC abc192\mainA.py

---
+++ abc192/mainA.py
@@ -0,0 +1,7 @@
+# A - Star
+# https://atcoder.jp/contests/abc192/tasks/abc192_a
+# 提出AC
+# https://atcoder.jp/contests/abc192/submissions/27595220
+x = int(input())
+ans = 100 - (x % 100)
+print(ans)
