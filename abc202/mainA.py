f61b76e8df148c76034a5696feff87afa0547c4e
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-29 21:43:45 +0900

    AC abc202\mainA.py

---
+++ abc202/mainA.py
@@ -0,0 +1,8 @@
+# A - Three Dice
+# https://atcoder.jp/contests/abc202/tasks/abc202_a
+# 提出AC
+# https://atcoder.jp/contests/abc202/submissions/27591397
+a,b,c = map(int, input().split())
+print((7-a) + (7-b) + (7-c))
+
+# さいころ
