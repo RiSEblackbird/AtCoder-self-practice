9c0aa1b2b86da9d2e403ca36718c5da2817ff93c
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-30 01:07:23 +0900

    AC abc193\mainA.py : 小数 百分率

---
+++ abc193/mainA.py
@@ -0,0 +1,9 @@
+# A - Discount
+# https://atcoder.jp/contests/abc193/tasks/abc193_a
+# 提出AC
+# https://atcoder.jp/contests/abc193/submissions/27594969
+a,b = map(float, input().split())
+ans = 100.0 - (b*100.0 / a) 
+print(ans)
+
+# 小数 百分率
