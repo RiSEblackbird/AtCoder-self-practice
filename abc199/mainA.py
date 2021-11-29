218712e9f4bb07deb06bad9eb0813054ecdbd29b
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-29 23:46:35 +0900

    AC abc199\mainA.py : べき乗

---
+++ abc199/mainA.py
@@ -0,0 +1,8 @@
+# A - Square Inequality
+# https://atcoder.jp/contests/abc199/tasks/abc199_a
+# 提出AC
+# https://atcoder.jp/contests/abc199/submissions/27593916
+a,b,c = map(int, input().split())
+print("Yes" if a**2 + b**2 < c**2 else "No")
+
+# べき乗
