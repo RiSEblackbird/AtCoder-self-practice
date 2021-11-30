5b6dadc31a08742dea19690f2b4104642124017e
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 00:41:18 +0900

    AC abc181\mainA.py 偶数 奇数

---
+++ abc181/mainA.py
@@ -0,0 +1,8 @@
+# A - Heavy Rotation
+# https://atcoder.jp/contests/abc181/tasks/abc181_a
+# 提出AC
+# https://atcoder.jp/contests/abc181/submissions/27607971
+n = int(input())
+print( "White" if n % 2 == 0 else "Black" )
+
+# 偶数 奇数
