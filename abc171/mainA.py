3a8b75e90ca58f45ccc7112deccba8e96bbc47a2
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 02:34:34 +0900

    AC abc171\mainA.py 大文字 小文字 str.islower()

---
+++ abc171/mainA.py
@@ -0,0 +1,9 @@
+# A - αlphabet
+# https://atcoder.jp/contests/abc171/tasks/abc171_a
+# 提出AC
+# https://atcoder.jp/contests/abc171/submissions/27608714
+a = input()
+print( "A" if a.islower() == False else "a" )
+
+# 大文字 小文字 str.islower()
+# https://docs.python.org/ja/3/library/stdtypes.html?highlight=join#str.islower
