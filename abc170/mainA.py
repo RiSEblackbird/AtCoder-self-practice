59fbca98cccd8c2e26d1c70baf649fc7903dd8c3
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 02:44:23 +0900

    AC abc170\mainA.py インデックス array.index()

---
+++ abc170/mainA.py
@@ -0,0 +1,9 @@
+# A - Five Variables
+# https://atcoder.jp/contests/abc170/tasks/abc170_a
+# 提出AC
+# https://atcoder.jp/contests/abc170/submissions/27608758
+a,b,c,d,e = map(int, input().split())
+ans = [a,b,c,d,e].index(0) + 1
+print(ans)
+
+# インデックス array.index()
--- 数値アレイ.py
+++ 数値アレイ.py
@@ -3,3 +3,6 @@
 
 ### array.append() アレイの末尾に要素を追加
 # https://docs.python.org/ja/3/library/array.html?highlight=append#array.array.append
+
+### array.index() 引数の要素が何番目であるかを取得
+# https://docs.python.org/ja/3/library/array.html?highlight=index#array.array.index
