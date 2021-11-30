4ea0a7f4e3add24bb71e67b4127328d140f20235
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 00:31:06 +0900

    AC abc182\mainA.py

---
+++ abc182/mainA.py
@@ -0,0 +1,6 @@
+# A - twiblr
+# https://atcoder.jp/contests/abc182/tasks/abc182_a
+# 提出AC
+# https://atcoder.jp/contests/abc182/submissions/27607874
+a,b = map(int, input().split())
+print( 2*a + 100 - b )
