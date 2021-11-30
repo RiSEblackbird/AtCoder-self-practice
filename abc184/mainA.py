0f5f320e09482a9962b149698fa549112799e8bb
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 00:09:10 +0900

    AC abc184\mainA.py 行列式

---
+++ abc184/mainA.py
@@ -0,0 +1,9 @@
+# A - Determinant
+# https://atcoder.jp/contests/abc184/tasks/abc184_a
+# 提出AC
+# https://atcoder.jp/contests/abc184/submissions/27607627
+a,b = map(int, input().split())
+c,d = map(int, input().split())
+print( a*d - b*c )
+
+# 行列式
