64b87283ed4d3ac6e9d39ab8fb26921b1dcde234
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-11-30 23:50:54 +0900

    AC abc186\mainA.py 切り捨て floor()

---
+++ abc186/mainA.py
@@ -0,0 +1,9 @@
+# A - Brick
+# https://atcoder.jp/contests/abc186/tasks/abc186_a
+# 提出AC
+# https://atcoder.jp/contests/abc186/submissions/27607299
+import math
+n,w = map(float, input().split())
+print( math.floor(n/w) )
+
+# 切り捨て floor()
---
+++ 数学関数.py
@@ -0,0 +1,5 @@
+##### math --- 数学関数 #####
+# https://docs.python.org/ja/3/library/math.html
+
+### 小数点切り捨て floor()
+# https://docs.python.org/ja/3/library/math.html#math.floor
