b836410df83832399ebe4cb3596b588a59164215
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 00:20:29 +0900

    AC abc183\mainA.py ReLU関数

---
+++ abc183/mainA.py
@@ -0,0 +1,8 @@
+# A - ReLU
+# https://atcoder.jp/contests/abc183/tasks/abc183_a
+# 提出AC
+# https://atcoder.jp/contests/abc183/submissions/27607753
+x = int(input())
+print( x if x >= 0 else 0 )
+
+# ReLU関数
