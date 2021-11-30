d070c4e86ed52496d5e92c0a4e98a60a1911c3e0
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 00:03:56 +0900

    AC abc185\mainA.py 最小値

---
+++ abc185/mainA.py
@@ -0,0 +1,8 @@
+# A - ABC Preparation
+# https://atcoder.jp/contests/abc185/tasks/abc185_a
+# 提出AC
+# https://atcoder.jp/contests/abc185/submissions/27607547
+a,b,c,d = map(int, input().split())
+print(min([a,b,c,d]))
+
+# 最小値
