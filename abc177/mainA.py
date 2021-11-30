c30b74df8c57be2eec2679d0747b47ca4b10c35a
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 01:13:08 +0900

    AC abc177\mainA.py 速度 距離

---
+++ abc177/mainA.py
@@ -0,0 +1,8 @@
+# A - Don't be late
+# https://atcoder.jp/contests/abc177/tasks/abc177_a
+# 提出AC
+# https://atcoder.jp/contests/abc177/submissions/27608237
+d,t,s = map(int, input().split())
+print( "Yes" if d <= t*s else "No")
+
+# 速度 距離
