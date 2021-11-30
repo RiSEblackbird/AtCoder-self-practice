39b339d6ac236a39e5f048bbd686ddbeffe75769
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 02:10:23 +0900

    AC abc174\mainA.py

---
+++ abc174/mainA.py
@@ -0,0 +1,6 @@
+# A - Air Conditioner
+# https://atcoder.jp/contests/abc174/tasks/abc174_a
+# 提出AC
+# https://atcoder.jp/contests/abc174/submissions/27608598
+x = int(input())
+print( "Yes" if x >= 30 else "No" )
