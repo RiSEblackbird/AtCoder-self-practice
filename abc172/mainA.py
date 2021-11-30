1081accdc91d4c32614c94839f4f0830c46d924a
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 02:23:59 +0900

    AC abc172\mainA.py

---
+++ abc172/mainA.py
@@ -0,0 +1,7 @@
+# A - Calc
+# https://atcoder.jp/contests/abc172/tasks/abc172_a
+# 提出AC
+# https://atcoder.jp/contests/abc172/submissions/27608674
+a = float(input())
+ans = a * (1 + a + a**2)
+print( int(ans) )
