36b7d94ad997b35f978e8e5163c15ad5eb8ea086
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-01 22:16:46 +0900

    AC ABC\abc169\mainA.rb

---
+++ ABC/abc169/mainA.rb
@@ -0,0 +1,6 @@
+# A - Multiplication 1
+# https://atcoder.jp/contests/abc169/tasks/abc169_a
+# 提出AC
+# https://atcoder.jp/contests/abc169/submissions/27618240
+a,b = map(int, input().split())
+print(a * b)
