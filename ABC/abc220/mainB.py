6d43f1ea315f4da9657701c0ea1626d742497dd4
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-03 01:05:38 +0900

    AC ABC\abc220\mainB.py 進数 N進数

---
+++ ABC/abc220/mainB.py
@@ -0,0 +1,10 @@
+# B - Base K
+# https://atcoder.jp/contests/abc220/tasks/abc220_b
+# 提出AC
+# https://atcoder.jp/contests/abc220/submissions/27633383
+k = int(input())
+a,b = map(str, input().split())
+print( int(a, k) * int(b, k) )
+# 進数の変換方法を見るために解答集をみた。
+
+# 進数 N進数
--- 数値操作.py
+++ 数値操作.py
@@ -6,3 +6,11 @@
 
 ### 総和 sum()
 # https://docs.python.org/ja/3/library/functions.html?highlight=sum#sum
+
+### 整数型 int()
+# https://docs.python.org/ja/3/library/functions.html#int
+# 進数の変換もできる 
+s = str(input())
+print(int(s, 2)) # 第一引数は文字列型であることに注意！
+# 11
+# 3
