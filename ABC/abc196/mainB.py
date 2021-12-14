2ea830cb816ed7aa0a0d43bebc9bd699ae594285
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-12-15 01:35:15 +0900

    AC ABC\abc196\mainB.py 丸め誤差 小数点切り捨て

---
+++ ABC/abc196/mainB.py
@@ -0,0 +1,7 @@
+# B - Round Down https://atcoder.jp/contests/abc196/tasks/abc196_b
+# 提出AC https://atcoder.jp/contests/abc196/submissions/27916249
+X = list(input().split("."))
+print( X[0] )
+
+# math.floorだと精度足りないので注意
+# 丸め誤差 小数点切り捨て
